f8424b139795e6715d85c8bda743778ad4caf449
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-03-09 10:32:10 +0000

    feat: Correct version of LTL evaluation and synchronous product type checking

--- src/LTL.hs
+++ src/LTL.hs
@@ -1,41 +1,63 @@
 module LTL (module LTL) where
 
 import Data.Graph (SCC(..), stronglyConnComp, flattenSCC)
-import Data.List (elemIndex, findIndices, elemIndices)
+import Data.List (elemIndex, findIndices, elemIndices, find)
+import Data.Maybe (catMaybes, mapMaybe)
 import qualified Data.Set as Set
 import Control.Parallel.Strategies
+import Debug.Trace
 
 import TransitionSystem
 
--- | Given a state in a Buchi automaton, return the state which satisfy one of the labels and are reachable from the state given or Nothing if no such state exists. This method assumes the automaton is deterministic.
-transitionByLabel :: [[Bool]] -> [Bool] -> Int -> Maybe Int
-transitionByLabel ba l priorState = elemIndex True reachableSatLabel
-  where
-    nextStates = ba !! priorState
-    reachableSatLabel = zipWith (&&) l nextStates `using` parList rseq
+data Buchi = Buchi {
+    buchiTS :: [[Maybe Char]]
+  , accepting :: [Int] -- Accepting indices
+  } deriving Show
 
+data Kripke = Kripke {
+    kripkeTS :: [[Bool]]
+  , kripkeLabel :: [(Int, [Char])] --given a state (Int) can get the props in that state
+  } deriving Show
 
--- | Given a Kripke structure and Buchi automaton, create the synchronous product
-createSynchronousProduct :: [[Bool]] -> [[Bool]] -> [Int] -> [[Bool]]
-createSynchronousProduct kripke buchi labelling = syn
-  where
-    syn = map (
-      \kripkeState -> map (\index ->
-        case transitionByLabel buchi (buchi !! index) kripkeState of
-          Nothing -> False
-          Just _ -> True
-        ) [0..length buchi-1] `using` parList rseq
-      ) [0..length kripke-1] `using` parList rseq
+getCommonElement :: Ord a => [a] -> [a] -> Bool
+getCommonElement xs ys = not $ Set.null $ Set.intersection (Set.fromList xs) (Set.fromList ys)
 
--- | Given a Kripke structure and a Buchi automaton, determine if the formula is refuted
-evaluateLTL :: [[Bool]] -> [[Bool]] -> [Int] -> Int -> (Bool, Maybe [Int])
-evaluateLTL transitionSystem buchi labelling initial = (refuted, prefix)
+getPropositionsPostKripke :: Kripke -> Int -> [Char]
+getPropositionsPostKripke k prior = concat propositions
   where
-    syn = createSynchronousProduct transitionSystem buchi labelling
+    posterior = elemIndices True $ post (kripkeTS k) prior
+    propositions = mapMaybe (\state -> lookup state (kripkeLabel k)) posterior
+
+synchronousProduct :: Kripke -> Buchi -> [[Bool]]
+synchronousProduct kripke buchi =
+  map
+    (\s ->
+      let
+        propositions = getPropositionsPostKripke kripke s
+      in
+        map
+          (\q ->
+             let
+               possibleBuchiTransitions = catMaybes $ post (buchiTS buchi) q
+             in
+               getCommonElement possibleBuchiTransitions propositions
+          ) [0..length (buchiTS buchi) -1]
+    ) [0..length (kripkeTS kripke)-1]
+
+evaluateLTL :: Kripke -> Buchi -> (Bool, Maybe [Int])
+evaluateLTL k b = (refuted, prefix)
+  where
+    syn = synchronousProduct k b
     bsccs = getBSCCs syn
-    reachableBsccs = depthFirstSearch syn [] initial
-    refuted = any (\bscc -> any (`elem` labelling) bscc && any (`elem` bscc) reachableBsccs) bsccs
+    reachableBsccs = depthFirstSearch syn [] 0
+    refuted = any (\bscc -> any (`elem` accepting b) bscc && any (`elem` bscc) reachableBsccs) bsccs
     prefix = if refuted then Just reachableBsccs else Nothing
+
+adjMatrixToEdges :: [[Bool]] -> [(Int, Int, [Int])]
+adjMatrixToEdges mat = [(i, i, adj i) | i <- nodes]
+  where
+    nodes = [0..length mat-1]
+    adj i = [ j | (j, True) <- zip [0..] (mat !! i) ]
 
 -- | Depth first search
 depthFirstSearch :: [[Bool]] -> [Int] -> Int -> [Int]
@@ -44,31 +66,6 @@ depthFirstSearch matrix visited vertex
   | otherwise = foldl (depthFirstSearch matrix) (vertex:visited) (reachableStates vertex) `using` parList rseq
   where
     reachableStates s = elemIndices True (matrix !! s)
-
--- Worse approach to computing satisfiability
-ndfs :: [[Bool]] -> [Int] -> Int -> [Int]
-ndfs matrix visited vertex
-  | vertex `elem` visited = vertex:visited
-  | otherwise = foldl (ndfs matrix) (vertex:visited) (reachableStates vertex) `using` parList rseq
-  where
-    reachableStates s = elemIndices True (matrix !! s)
-
--- | Depth first search that runs ndfs if vertex is in accepting
-dfs :: [[Bool]] -> [Int] -> [Int] -> Int -> [Int]
-dfs matrix accepting visited vertex
-  | vertex `elem` visited = visited
-  | otherwise =
-     if vertex `elem` accepting && length (ndfs matrix [] vertex) > 1 then ndfs matrix [] vertex else foldl (dfs matrix accepting) (vertex:visited) (reachableStates vertex) `using` parList rseq
-  where
-    reachableStates s = elemIndices True (matrix !! s)
-
--- | Given a Kripke structure and a Buchi automaton, get if an accepting cycle exists
-detectAcceptingCycles :: [[Bool]] -> [Int] -> Int -> Bool
-detectAcceptingCycles matrix accepting initial = cycle
-  where
-    result = dfs matrix accepting [] initial
-    cycle = length result > 1 && head result == result !! (length result -1)
-
 -- | Convert a adjacency matrix to a graph
 adjMatrixToGraph :: [[Bool]] -> [(Int, Int, [Int])]
 adjMatrixToGraph mat = [(i, i, adj i) | i <- nodes]
@@ -97,3 +94,30 @@ getBSCCs :: [[Bool]] -> [[Int]]
 getBSCCs m = filter (`closed` m) sccs
   where
     sccs = getSCCs m
+
+-- evaluateLTL :: Eq a => [[Bool]] -> BuchiState a -> [[a]] -> (Bool, Maybe [Int])
+-- evaluateLTL transitionSystem buchi labelling = (refuted, prefix)
+--   where
+--     syn = synchronousProduct transitionSystem labelling buchi
+--     initial = findIndices initial buchi
+--     accepting = findIndices accepting buchi
+--     bsccs = getBSCCs syn
+--     reachableBsccs = depthFirstSearch syn [] initial
+--     refuted = any (\bscc -> any (`elem` labelling) bscc && any (`elem` bscc) reachableBsccs) bsccs
+--     prefix = if refuted then Just reachableBsccs else Nothing
+-- 
+-- generateBuchiStates :: [BuchiState Char]
+-- generateBuchiStates = state0 : [state1]
+--   where
+--     state0 = BuchiState {
+--       transitions = [('b', state0),('a', state1)],
+--       accepting = False,
+--       initial = True,
+--       index = 0
+--   }
+--     state1 = BuchiState {
+--         transitions = [('a', state1)],
+--         accepting = True,
+--          initial = False,
+--         index = 1
+--     }
